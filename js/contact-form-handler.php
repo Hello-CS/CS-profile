ae7b2d5542a05e4df6c6951bf6404cd4ec4ccd09
Hello-CS <mrlamchensian@gmail.com>
2021-03-06 17:42:06 +0800

    Adding mail feature

---
+++ js/contact-form-handler.php
@@ -0,0 +1,13 @@
+<?php
+if($_POST["submit"]) {
+    $recipient="mrlamchensian@gmail.com"; //Enter your mail address
+    $subject=$_POST["subject"]; //Subject 
+    $sender=$_POST["name"];
+    $senderEmail=$_POST["email"];
+    $message=$_POST["message"];
+    $mailBody="Name: $sender\nEmail Address: $senderEmail\n\nMessage: $message";
+    mail($recipient, $subject, $mailBody);
+    sleep(1);
+    header("Location:http://hello-cs.github.io/CS-profile"); // Set here redirect page or destination page
+}
+?>
